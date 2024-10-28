d42d32ce0e551e8fe261af67becc1447e3640f54
Sandro Seivane <seivanesandro@gmail.com>
2024-10-28 19:13:05 +0000

    correct some text onn project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
     return (
         <div className="App">
-            project Meal Planner
+            project Meal Planer
         </div>
     );
 }
