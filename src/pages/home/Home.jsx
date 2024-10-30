aa1d0a831e89dfc5103a2665a57a19e99d34d889
Sandro Seivane <seivanesandro@gmail.com>
2024-10-30 13:21:59 +0000

    use prettier to indent code

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 //import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import styled, { keyframes } from 'styled-components';
-import hero from '../../assets/hero.jpg'
+import styled, {
+    keyframes
+} from 'styled-components';
+import hero from '../../assets/hero.jpg';
 import { devices } from '../../utils/constantes';
 
 const Show = keyframes`
@@ -32,7 +34,6 @@ const ContainerHero = styled.div`
     background-position: center !important;
     background-size: cover !important;
     width: 100%;
-
 `;
 const ContainerHeroBanner = styled.div`
     display: flex;
@@ -83,74 +84,74 @@ const StyledHeroParagraph = styled.p`
 `;
 
 const Home = props => {
-    const [ search, setSearch ] = useState('');
+    const [search, setSearch] = useState('');
 
     const getSearch = () => {
         setSearch(search);
-    }
+    };
 
-  return (
-      <>
-          <header className="home">
-              <ContainerHero className="hero">
-                  <ContainerHeroBanner className="hero-banner">
-                      <StyledHeroTitle className="outhero-title">
-                          Meal planner
-                      </StyledHeroTitle>
-                      <StyledHeroParagraph className="hero-paragraph">
-                          "Tell me what you eat,
-                          and I will tell you who
-                          you are.."
-                      </StyledHeroParagraph>
-                      <Form
-                          style={{
-                              display: 'flex',
-                              flexDirection:
-                                  'column',
-                              justifyContent:
-                                  'center',
-                              alignItems:
-                                  'center',
-                              gap: '2rem'
-                          }}
-                      >
-                          <Form.Control
-                              type="search"
-                              placeholder="Search..."
-                              variant="dark"
-                              className="hero-input text-center me-2 rounded-4"
-                              aria-label="Search"
-                              onChange={e => {
-                                  setSearch(
-                                      e.target
-                                          .value
-                                  );
-                              }}
-                          />
-                          <Button
-                              variant="light"
-                              className="hero-btn px-5 rounded-4 shadow text-dark"
-                              style={{
-                                  fontWeight:
-                                      '500'
-                              }}
-                              onClick={() => {
-                                  getSearch();
-                                  console.log(
-                                      search
-                                  );
-                              }}
-                          >
-                              Search
-                          </Button>
-                      </Form>
-                  </ContainerHeroBanner>
-              </ContainerHero>
-          </header>
-      </>
-  );
-}
+    return (
+        <>
+            <header className="home">
+                <ContainerHero className="hero">
+                    <ContainerHeroBanner className="hero-banner">
+                        <StyledHeroTitle className="outhero-title">
+                            Meal planner
+                        </StyledHeroTitle>
+                        <StyledHeroParagraph className="hero-paragraph">
+                            "Tell me what you eat,
+                            and I will tell you
+                            who you are.."
+                        </StyledHeroParagraph>
+                        <Form
+                            style={{
+                                display: 'flex',
+                                flexDirection:
+                                    'column',
+                                justifyContent:
+                                    'center',
+                                alignItems:
+                                    'center',
+                                gap: '2rem'
+                            }}
+                        >
+                            <Form.Control
+                                type="search"
+                                placeholder="Search..."
+                                variant="dark"
+                                className="hero-input text-center me-2 rounded-4"
+                                aria-label="Search"
+                                onChange={e => {
+                                    setSearch(
+                                        e.target
+                                            .value
+                                    );
+                                }}
+                            />
+                            <Button
+                                variant="light"
+                                className="hero-btn px-5 rounded-4 shadow text-dark"
+                                style={{
+                                    fontWeight:
+                                        '500'
+                                }}
+                                onClick={() => {
+                                    getSearch();
+                                    console.log(
+                                        search
+                                    );
+                                }}
+                            >
+                                Search
+                            </Button>
+                        </Form>
+                    </ContainerHeroBanner>
+                </ContainerHero>
+            </header>
+        </>
+    );
+};
 
-Home.propTypes = {}
+Home.propTypes = {};
 
-export default Home
+export default Home;
